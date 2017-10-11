fd75e6cf778e3ff68cf501bce8d788789cb7b573
Eric <deviljiang021@gmail.com>
2017-10-11 19:00:53 +0800

    10_11_2

--- src/main.js
+++ src/main.js
@@ -33,6 +33,9 @@ Date.prototype.Format = function (fmt) { // author: meizz
     if (new RegExp("(" + k + ")").test(fmt)) fmt = fmt.replace(RegExp.$1, (RegExp.$1.length == 1) ? (o[k]) : (("00" + o[k]).substr(("" + o[k]).length)));
   return fmt;
 }
+router.afterEach((to, from, next) => { // 自动滚动至页面页首
+  window.scrollTo(0, 0);
+});
 Vue.filter("datefmt", function (date) {
   // debugger
   var fmt = 'yyyy-MM-dd';
